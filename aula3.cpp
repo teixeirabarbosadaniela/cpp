882efb5e91840e38e67d99494496db1a978002b8
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-08-20 17:56:45 -0300

    Limpeza do repositório, aplicando .gitignore

---
+++ aula3.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout <<"mais uma linguagem" << endl;//no lugar de int, poderia ser tipo de retorno void ou bool// 
+    system("pausa"); //inclui uma pausa, pressionando qualquer tecla ele ativa//
+
+    return 0; 
+    //
--- main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Olá, Daniela!" << std::endl;
-    return 0;
-}
