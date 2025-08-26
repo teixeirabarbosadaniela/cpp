9d54cd180cb56916e6674ec415da3cc766ea38b9
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-08-26 16:18:20 -0300

    Removendo arquivos aula3.cpp, aula4.cpp, main.cpp e teste.cpp

--- aula3.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    cout <<"mais uma linguagem" << endl;//no lugar de int, poderia ser tipo de retorno void ou bool// 
-    system("pausa"); //inclui uma pausa, pressionando qualquer tecla ele ativa//
-
-    return 0; 
-    
