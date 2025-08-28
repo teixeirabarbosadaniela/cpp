9a45438564d4e9513645dd4687c8acee63ee2c09
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-08-28 18:45:17 -0300

    adicionando novas aulas

---
+++ aula06.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int n1, n2; //variáveis globais (escopo global, podem ser acessadas de qualquer lugar - mesmo dentro da main!)
+
+int main() {
+
+    // operadores matemáticos: ()/ * + - % 
+
+    int n3, n4; //variáveis locais, não poderão ser acessadas por outras funções (escopo local)
+    int res;
+
+    n1=11;
+    n2=3;
+    n3=5;
+    n4=2;
+
+    res=n1+n2+n3+n4;
+
+    cout << "soma de todas as variáveis: " << res << "\n\n";
+    
+
+    return 0;
+}
