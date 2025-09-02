74eab74948a850ee63ff2035d959f7c9e3b0208d
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-09-02 18:53:37 -0300

    atualização gitignore

---
+++ aula07.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int n1,n2;
+
+    n1=0;
+    n2=10;
+
+    //incremento e decremento
+
+    cout << n1 << "\n\n"; //antes do incremento
+    n1=n1+1; //forma tradicional
+    cout << n1 << "\n\n"; //depois do incremento
+
+    
+    
+
+    return 0;
+}
