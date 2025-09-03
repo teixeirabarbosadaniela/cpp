80bba23632af703fb7352b700b46cedf824ddab7
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-09-03 14:58:19 -0300

    aula 26

---
+++ aula36.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+//ponteiros armazenam o endereço de outra variável/ está apontando o endereço de outra variável
+
+    string veiculo="carro";
+
+    string *pv;
+    pv=&veiculo; //ponteiro pveiculo recebe o endereço da variável veículo
+
+    cout << pv << "\n\n" << &veiculo;
+
+    *pv="Moto"; //no endereço apontado por pveiculo, adicione "moto"
+
+    cout << "\n\n" << veiculo << "\n\n" << *pv;
+
+
+
+
+
+
+/*
+int *pn; // operador de ponteiro *
+pn= &num; //operador de endeço &
+
+cout << pn "\n\n";
+*/
+
+
+
+
+    return 0;
+}
