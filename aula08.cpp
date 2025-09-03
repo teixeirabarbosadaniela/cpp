5eb071baea63548a94712b5733f8fdb912aebbb1
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-09-03 14:29:01 -0300

    aula08 completa

--- aula08.cpp
+++ aula08.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 using namespace std;
 int main() {
+
+    int num=10;
+
+    cout << num << "\n\n";
+
+    //método tradicional / multiplica por -1 ou operador pra inversão (sinal de menos)
+
+    //num=num*-1; ou simplificada definitivo de inversão num=-num;
+
+    cout << -num << "\n\n"; //metodo alternativo não grava valor na variável, apenas inverte momentaneamente
+    
+
+
     return 0;
 }
