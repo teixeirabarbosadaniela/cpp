c892675a779bc151036f56afefdc7fb01654f00d
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-09-03 11:32:51 -0300

    Atualização aula07.cpp (corrigido)

--- aula07.cpp
+++ aula07.cpp
@@ -2,19 +2,34 @@
 using namespace std;
 int main() {
 
-    int n1,n2;
-
-    n1=0;
-    n2=10;
+    int n1=10; //declaramos e inicializamos variável ao mesmo tempo
+    
 
     //incremento e decremento
 
     cout << n1 << "\n\n"; //antes do incremento
-    n1=n1+1; //forma tradicional
-    cout << n1 << "\n\n"; //depois do incremento
 
-    
-    
+
+
+    //n1=n1+1; //forma tradicional incremento
+    //n1=n1-5; //forma tradicional decremento
+
+    cout << n1++ << "\n\n"; //pós-incremento 
+    cout << ++n1 << "\n\n"; //pré-incremento
+
+    //n1+=1; //forma simplificada/contraída de incrementar
+    //n1-=5; //forma simplificada/contraída de decrementar
+
+    //n1++; //outra maneira incremento (soma sempre apenas 1)
+    //n1--; //outra maneira de decremento (subtrai sempre apenas 1)
+
+    //n1*=2; //multiplica n1 por 2
+    //n1%=2; //mostra o resto da divisão por 2
+    //n1/=2; //divide por 2
+
+    //n1++; pós-incremento ou pós-fixado
+    //++n1; pré-incremento ou pré-fixado
+
 
     return 0;
 }
