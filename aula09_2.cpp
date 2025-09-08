21c0c2f6b014a3ab41450915f922e55396ea51fe
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-09-08 07:52:55 -0300

    aula09_2 atualizada

--- aula09_2.cpp
+++ aula09_2.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
-uising namespace std;
+using namespace std;
 int main() {
+
+
+    int n1,n2,res;
+    char opc;
+
+    cout << "Digite o valor da nota 1: ";
+    cin >> n1;
+
+    cout << "Digite o valor da nota 2: ";
+    cin >> n2;
+
+    res=n1+n2;
+
+    if(res >= 60) {
+        cout << "\nAluno aprovado\n";
+    }else {
+        cout << "\nAluno reprovado\n";
+    }
 
     return 0;
 }
