6b5bd308fb5404ea4b4af36d9d4ddc7f46ee4d1f
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-09-08 08:19:57 -0300

    update correcoes

---
+++ aula10.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main() {
+
+    int n1,n2,res;
+    char opc;
+
+    inicio: //lable
+    
+    system("cls"); //comando do DOS para limpar a tela pós loop e, pra ele funcionar, precisa adc biblioteca <cstdlib>""
+
+    cout << "Digite o valor da nota 1: ";
+    cin >> n1;
+
+    cout << "Digite o valor da nota 2: ";
+    cin >> n2;
+
+    res=n1+n2;
+
+/*  
+    >=60 Aprovado
+    >=40 e <60 Recuperação
+    <40 Reprovado
+
+*/
+
+    if(res >= 60) {
+        cout << "\nAluno aprovado\n";
+    }else if(res >=40) {
+        cout << "\nRecuperação\n";
+    }else {
+        cout << "\nAluno reprovado\n";
+    }
+
+    cout << "\nDigitar outras notas? (s/n): \n";
+    cin >> opc;
+
+    if(opc=='s' or opc=='S') {
+        goto inicio;
+    }
+ 
+    return 0;
+}
