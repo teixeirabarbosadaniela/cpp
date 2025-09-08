d82fb117fc8cffd264d2fe34ab3c611e7c722140
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-09-08 07:42:34 -0300

    aula09_2

---
+++ aula09_1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    int num1=10;
+    int num2=50;
+    int sol=1;
+
+
+    if(sol >= 10){
+        cout << "vai dar praia";
+    }else{
+        cout << "fico em casa";
+    }
+
+/*
+    if(num1>num2) {
+        cout << "TRUE"; //1 == se colocar apenas o nome da variável, considera-se verdadeiro(1)
+    }
+    else{
+        cout << "FALSE"; //0
+    }
+
+*/    
+
+
+/*
+    if(teste logico) {
+        //se o teste for verdadeiro
+       
+    }else {//caso contrário
+
+    //se o teste for falso
+
+    }
+
+    //operadores de comparação: <,>,>=,<=,==,!=
+*/
+
+
+    return 0;
+}
---
+++ aula09_2.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+uising namespace std;
+int main() {
+
+    return 0;
+}
