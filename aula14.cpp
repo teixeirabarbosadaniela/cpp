24f44cdd01b3c938e6c8a697ba69ce709b6f556a
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-09-16 18:28:56 -0300

    update aula14

--- aula14.cpp
+++ aula14.cpp
@@ -10,6 +10,43 @@ while(expressao){
 }
 */
 
+/*
+    int n;
+
+    n=0;
+    while(n<10){
+        cout << n << "\n";
+        //n++; //concede uma condição de parada
+        cin >> n; //pode ser recebendo do teclado
+        
+    }
+    */
+
+    /*
+    int cont;
+    cont=0;
+    while(cont<20){
+        cout << cont << "\nI will not copy code without understanding it";
+        cont++; //condição de parada   ou de dois em dois 'cont+=2' (suscessivamente)     
+    }
+    */
+
+    
+    int cont;
+    cont=0;
+    while(cont++<20){//incremento para condição de parada  
+        cout << cont << "\n" << "I will not copy code without understanding it - ";
+
+        if(cont == 10) {
+            break;
+        }
+           
+    }
+    
+
+    cout << "\nRotina finalizada\n";
+   
+
 
 
 
