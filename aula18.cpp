9f0b8f78a099a29be67ffb3dfdcfffb35bbce2cc
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-09-22 18:30:45 -0300

    aula18 inicio

---
+++ aula18.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+    return 0;
+}
+
+/*
+tema: ARRAY ou VETORES: coleção de variaveis(posições) para armazenar valores de mesmo tipo (declara informando tipo, nome, tamanho)
+vetor simples (unidirecional) por enquanto.
+*/
+
