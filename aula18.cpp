e1ec652eafd6e180aaa6059e7ee1c7b7b73aef13
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-09-22 18:47:55 -0300

    continuacao aula18

--- aula18.cpp
+++ aula18.cpp
@@ -7,15 +7,23 @@ tema: ARRAY ou VETORES: coleção de variaveis(posições) para armazenar valore
 vetor simples (unidirecional) por enquanto.
 */
 
-
+    /*
     int vetor[5]; // seria o mesmo que descrever 'int vetor0, vetor1, vetor2, ...vetor4' (porque iniciei com zero (sempre!) e solicitei 5 posições(tamanho));
+    int i;
     vetor[0]=10;
     vetor[1]=20;
     vetor[2]=30;
     vetor[3]=40;
     vetor[4]=50;
 
-    cout << vetor[0] << "\n";
+    for(i=0; i<5; i++) {//for é apropriado pra loop de array
+    cout << vetor[i] << "\n"; 
+    }
+    */
+
+    
+
+    //função sizeof, pra facilitar o trabalho e nao precisar atualizar sempre / retorna o tamanho do tipo ---> duvida: pq divide por 4 e continua funcionando!
 
 
     return 0;
