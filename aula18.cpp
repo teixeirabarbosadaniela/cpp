e7c6222c86170075ee008779a2b912934d52a77a
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-09-23 14:39:35 -0300

    update aula18a

--- aula18.cpp
+++ aula18.cpp
@@ -7,7 +7,7 @@ tema: ARRAYS ou VETORES: coleção de variaveis(posições) para armazenar valor
 vetor simples (unidirecional) por enquanto.
 */
 
-    /*
+    
     int vetor[5]; // seria o mesmo que descrever 'int vetor0, vetor1, vetor2, ...vetor4' (porque iniciei com zero (sempre!) e solicitei 5 posições(tamanho));
     int i;
     vetor[0]=10;
@@ -19,18 +19,22 @@ vetor simples (unidirecional) por enquanto.
     for(i=0; i<5; i++) {//for é apropriado pra loop de array
         cout << vetor[i] << "\n"; 
     }
-    */
+    
 
     //função sizeof retorna tamanho.
     //pra facilitar o trabalho em relação ao tamanho do array e nao precisar atualizar sempre / retorna o tamanho do tipo ---> duvida: pq divide por 4 e continua funcionando!
 
+
+    /* revisar:
+    
         int tam=5;
         int vetor[tam]={10,20,30,40,50};//cria e insere elementos ao mesmo tempo. obs.: na aula não fica claro como ajustar no código para atualizar automaticamente/ revisar 
         int i
 
-    
         for(i=0; i<tam; i++) { //automatiza a atualização dos valores 
             cout << vetor[i] << "\n";
         }
+
+    */
     return 0;
 }
