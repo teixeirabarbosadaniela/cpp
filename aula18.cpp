da50723f789c91d9896bac998f744b8246174e11
Daniela Teixeira Barbosa <teixeirabarbosadaniela@gmail.com>
2025-09-23 14:37:54 -0300

    updates aula18

--- aula18.cpp
+++ aula18.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
 
 /*
-tema: ARRAY ou VETORES: coleção de variaveis(posições) para armazenar valores de mesmo tipo (declara informando tipo, nome, tamanho)
+tema: ARRAYS ou VETORES: coleção de variaveis(posições) para armazenar valores de mesmo tipo (declara informando tipo, nome, tamanho)
 vetor simples (unidirecional) por enquanto.
 */
 
@@ -17,14 +17,20 @@ vetor simples (unidirecional) por enquanto.
     vetor[4]=50;
 
     for(i=0; i<5; i++) {//for é apropriado pra loop de array
-    cout << vetor[i] << "\n"; 
+        cout << vetor[i] << "\n"; 
     }
     */
 
+    //função sizeof retorna tamanho.
+    //pra facilitar o trabalho em relação ao tamanho do array e nao precisar atualizar sempre / retorna o tamanho do tipo ---> duvida: pq divide por 4 e continua funcionando!
+
+        int tam=5;
+        int vetor[tam]={10,20,30,40,50};//cria e insere elementos ao mesmo tempo. obs.: na aula não fica claro como ajustar no código para atualizar automaticamente/ revisar 
+        int i
+
     
-
-    //função sizeof, pra facilitar o trabalho e nao precisar atualizar sempre / retorna o tamanho do tipo ---> duvida: pq divide por 4 e continua funcionando!
-
-
+        for(i=0; i<tam; i++) { //automatiza a atualização dos valores 
+            cout << vetor[i] << "\n";
+        }
     return 0;
 }
